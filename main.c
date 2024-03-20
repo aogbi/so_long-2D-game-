f747f4bd2cdb6644b2648885914a59be82de917f
aogbi <ayoub.ogbi14@gmail.com>
2024-03-20 05:01:39 +0000

    map cheker is done

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include "so_long.h"
 
-// void	render_background(t_data *data, int color)
-// {
-//     int	i;
-//     int	j;
+// // void paste_part_into_image(t_img *img1, t_img *img2, t_action action, int size_width, int size_height) 
+// // {
+// // 	int j;
+// // 	int i = action.y;
+// // 	int h = 0;
 
-//     if (data->win_ptr == NULL)
-//         return ;
-//     i = 0;
-//     while (i < WINDOW_HEIGHT)
-//     {
-//         j = 0;
-//         while (j < WINDOW_WIDTH)
-//             mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
-//         ++i;
-//     }
-// }
+// // 	while (i < img1->height)
+// // 	{
+// // 		j = 0;
+// // 		while (j < img1->width)
+// // 		{
+// // 			if ((int)&img1->addr[(j + (i * img1->width)) * 4] != 0)
+// // 				img2->addr[(action.x + (action.y * img2->width)) + j] = img1->addr[j + (i * img1->line_len)];
+// // 			j++;
+// // 		}
+// // 		action.y++;
+// // 		i++;
+// // 	}
+// // }
+
 void copy_part_of_image(t_img *img1, t_img *img2, t_action action, int size_width, int size_height) 
 {
 	int j; 
@@ -37,45 +41,16 @@ void copy_part_of_image(t_img *img1, t_img *img2, t_action action, int size_widt
 		i++;
 	}
 }
-void paste_part_into_image(t_img *img1, t_img *img2, t_action action, int size_width, int size_height) 
-{
-	int j;
-	int i = action.y;
-	int h = 0;
 
-	while (i < img1->height)
+int main(int argc, char **argv)
+{
+	t_rules rules;
+
+	rules = read_map(open_file(argc, argv));
+	if (rules.map_valid && rules.map_valid != -1)
 	{
-		j = 0;
-		while (j < img1->width)
-		{
-			if ((int)&img1->addr[(j + (i * img1->width)) * 4] != 0)
-				img2->addr[(action.x + (action.y * img2->width)) + j] = img1->addr[j + (i * img1->line_len)];
-			j++;
-		}
-		action.y++;
-		i++;
-	}
-}
-
-void valid(int arc, char **arv)
-{
-	char *map;
-    int fd;
-
-    if (arc != 2) {
-        printf("Usage: %s <map>\n", arv[0]);
-        return 1;
+        ft_printf("map machi s7i7a\n");
+        return (0);
     }
-    map = arv[1];
-    fd = open(map, O_RDONLY);
-    if (fd == -1) {
-        perror("Error opening file");
-        return 1;
-    }
-    close(fd);
-}
-int main(int argc, char *argv[]) 
-{   
-    
     return 0;
 }
--- so_long.h
+++ so_long.h
@@ -1,23 +1,32 @@
 #ifndef SO_LONG_H
 #define SO_LONG_H
 
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-
-
 #include <X11/X.h>
 #include <X11/keysym.h>
 #include <math.h>
 #include "minilibx-linux/mlx.h"
 #include "get_next_line/get_next_line.h"
+#include <string.h>
+#include "ft_printf/ft_printf.h"
+#include "libft/libft.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
 #define MLX_ERROR 1
+
+typedef struct s_rules
+{
+    char *map;
+    int len;
+    int height;
+	int p;
+    int p_index;
+	int e;
+    int e_index;
+	int c;
+    int map_valid;
+}   t_rules;
 
 typedef struct s_img
 {
